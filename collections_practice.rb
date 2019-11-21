661709e5e98abc11fda93f1cc213d97789672544
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-21 11:03:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,14 +17,11 @@ end
 
 def reverse_array(array)
   array.reverse
-  return array 
+  return array
 end
 
 def kesha_maker(array)
   array.each do |name|
   name[2] = "$"
   return array
-end 
-
-
-
+end
