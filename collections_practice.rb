075164ffe04f51fb194726fecf1e08f79fc893f5
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-21 10:23:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc (array)
+   return array.sort{|x,y| x <=> y }
+end
 
+def sort_array_desc(array)
+   return array.sort.reverse()
+end
+
+def sort_array_char_count(array)
+  return array.sort {|x,y| x.length <=> y.length }
+end
+
+def swap_elements(array)
+  return array = array[0,2,1]
+end
